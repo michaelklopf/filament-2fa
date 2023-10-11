1ed4c93af9f297ea6d8025820cb9a221cdc592fd
Michael <michael.klopf@suenert.de>
2023-10-11 11:47:18 +0200

    Change 2fa route to not collide with jetstream

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::domain(config('filament.domain'))
     ->middleware(config('filament.middleware.base'))
     ->prefix(config('filament.path'))
     ->group(function () {
-        Route::get('/two-factor-challenge', config('filament-2fa.two_factor_challenge_component_path'))
+        Route::get('/filament-two-factor-challenge', config('filament-2fa.two_factor_challenge_component_path'))
             ->name('filament-2fa.login');
     });
+ 
